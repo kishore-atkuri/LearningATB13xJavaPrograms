6d610fae2c7df7676baec59d8c3705a1de92d5f3
KISHORE BABU <kishorebabu08@gmail.com>
2025-06-10 16:56:31 +0530

    Commit changes

--- src/ex_01_Java_Basics/Lab002_NoMainMethod.java
+++ src/ex_01_Java_Basics/Lab002_NoMainMethod.java
@@ -2,8 +2,7 @@ package ex_01_Java_Basics;
 
 public class Lab002_NoMainMethod {
     public static void main(String[] args) {
-
-        System.out.println("Mi");
+        System.out.println();
 
     }
 
