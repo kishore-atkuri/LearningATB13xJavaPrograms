e56f06a5fe43be2a0ec2f35d37be7825898ff3ee
KISHORE BABU <kishorebabu08@gmail.com>
2025-06-13 06:56:25 +0530

    Day4 commits

---
+++ src/ex_02_Java_Basics/Lab014_Variables.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Lab014_Variables {
+    // Main program with multiple main methods allowed but main(String[] args) should be only one
+    public static void main(String[] args) {
+        System.out.println("Main program with multiple main method");
+    }
+
+    public static void main(int args) {
+
+    }
+
+    public static void main() {
+
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab016_Variables_int.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics;
+
+public class Lab016_Variables_int {
+    public static void main(String[] args) {
+        int a = 10;
+        a = 65 + 1;
+        System.out.println("Int a value is "+a);
+    }
+}
