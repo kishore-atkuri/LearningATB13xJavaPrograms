e8456121d7dd45c54cfbf68bd8ed37431f8f8800
KISHORE BABU <kishorebabu08@gmail.com>
2025-07-12 10:18:51 +0530

    12th June 2025 Commits1

---
+++ src/ex_06_Ternary_Operator/Lab063_TO_P1.java
@@ -0,0 +1,10 @@
+package ex_06_Ternary_Operator;
+
+public class Lab063_TO_P1 {
+    public static void main(String[] args) {
+        // Is Number: positive or negative
+        int num = -5;
+        String result = num > 0 ? "Positive" : "Negative";
+        System.out.println(result);
+    }
+}
