f6f3875d22d3e9e4d598986506146288a0e77657
KISHORE BABU <kishorebabu08@gmail.com>
2025-07-13 18:32:43 +0530

    22nd June 2025 Commits

---
+++ src/ex_14_Strings/Lab135_String_Immutable.java
@@ -0,0 +1,10 @@
+package ex_14_Strings;
+
+public class Lab135_String_Immutable {
+    public static void main(String[] args) {
+        String name = "Kishore"; // SCP
+//        name.toUpperCase();
+        name = name.toUpperCase(); // KISHORE
+        System.out.println(name);
+    }
+}
